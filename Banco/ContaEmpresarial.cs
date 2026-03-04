c5ed912c0e07b48452f71a76bffd718c8f8eb6a1
Cleiton Da Silva Batista <cleitondasilvabatista11@gmail.com>
2026-03-04 08:46:26 -0300

    Refactor ContaEmpresarial to use account number and balance

--- Banco/ContaEmpresarial.cs
+++ Banco/ContaEmpresarial.cs
@@ -1,18 +1,29 @@
-﻿class ContaEmpresarial : ContaBancaria
+class ContaEmpresarial : ContaBancaria
 {
     public decimal LimiteEmprestimo { get; private set; } = 10000m;
 
-    public ContaEmpresarial(string holder, decimal balance)
-        : base(holder, balance)
+    public ContaEmpresarial(int numeroConta, string titular, decimal saldo)
+        : base(numeroConta, titular, saldo)
     {
     }
 
     public override bool Sacar(decimal valor)
     {
-        if (valor <= 0 || valor > Balance + LimiteEmprestimo)
+        if (valor <= 0 || valor > Saldo + LimiteEmprestimo)
             return false;
 
-        Balance -= valor;
+        Saldo -= valor;
+        return true;
+    }
+
+    public bool FazerEmprestimo(decimal valor)
+    {
+        if (valor <= 0 || valor > LimiteEmprestimo)
+            return false;
+
+        Saldo += valor;
+        LimiteEmprestimo -= valor;
+
         return true;
     }
 }
